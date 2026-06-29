3683ed93541cc6c35cd84aa649e6f5b7aca4ff5f
sumon550 <entertainmentcharacter54321@gmail.com>
2026-06-29 18:28:05 +0600

    Add ebook details main page

--- src/app/ebook/[id]/page.jsx
+++ src/app/ebook/[id]/page.jsx
@@ -1,13 +1,13 @@
-// import EbookDetails from "./EbookDetails";
+import EbookDetails from "./EbookDetails";
 
-// // Next.js 15 ডাইনামিক পেজ র্যাপার
-// export default async function EbookDetailsPage({ params }) {
-//   // অ্যাসিনক্রোনাসলি আইডি ডিস্ট্রাকচার করা
-//   const { id } = await params;
+// Next.js 15 ডাইনামিক পেজ র্যাপার
+export default async function EbookDetailsPage({ params }) {
+  // অ্যাসিনক্রোনাসলি আইডি ডিস্ট্রাকচার করা
+  const { id } = await params;
 
-//   return (
-//     <main className="min-h-screen bg-slate-950 text-slate-100 pt-20 pb-12">
-//       <EbookDetails bookId={id} />
-//     </main>
-//   );
-// }
+  return (
+    <main className="min-h-screen bg-slate-950 text-slate-100 pt-20 pb-12">
+      <EbookDetails bookId={id} />
+    </main>
+  );
+}
